b5e488ada03d169e10bec04af234324673c11bf0
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-09-10 06:56:32 +0200

    Finished the navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
+import Nav from "./Nav";
 const header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Luke <span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default header;
